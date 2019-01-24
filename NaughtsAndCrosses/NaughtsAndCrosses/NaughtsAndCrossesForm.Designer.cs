705a8a7bd182e2281d9c029b4fdfdbca8c8c4745
lauraljh25 <39875455+lauraljh25@users.noreply.github.com>
2019-01-24 18:53:45 +0000

    add all associated files

---
+++ NaughtsAndCrosses/NaughtsAndCrosses/NaughtsAndCrossesForm.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace NaughtsAndCrosses
+{
+    partial class NC
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.lbl5 = new System.Windows.Forms.Label();
+            this.lbl6 = new System.Windows.Forms.Label();
+            this.lbl7 = new System.Windows.Forms.Label();
+            this.lbl8 = new System.Windows.Forms.Label();
+            this.lbl9 = new System.Windows.Forms.Label();
+            this.lbl = new System.Windows.Forms.Label();
+            this.player1Btn = new System.Windows.Forms.Button();
+            this.player2Btn = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.Controls.Add(this.lbl2, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.lbl3, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.lbl4, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.lbl5, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lbl6, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lbl7, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lbl8, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lbl9, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lbl, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(425, 355);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Visible = false;
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.BackColor = System.Drawing.SystemColors.Control;
+            this.lbl2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbl2.Font = new System.Drawing.Font("Mistral", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.Location = new System.Drawing.Point(286, 237);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(135, 117);
+            this.lbl2.TabIndex = 8;
+            this.lbl2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl2.Visible = false;
+            this.lbl2.Click += new System.EventHandler(this.label_click);
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.BackColor = System.Drawing.SystemColors.Control;
+            this.lbl3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbl3.Font = new System.Drawing.Font("Mistral", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl3.Location = new System.Drawing.Point(145, 237);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(134, 117);
+            this.lbl3.TabIndex = 7;
+            this.lbl3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl3.Visible = false;
+            this.lbl3.Click += new System.EventHandler(this.label_click);
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.BackColor = System.Drawing.SystemColors.Control;
+            this.lbl4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbl4.Font = new System.Drawing.Font("Mistral", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl4.Location = new System.Drawing.Point(4, 237);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(134, 117);
+            this.lbl4.TabIndex = 6;
+            this.lbl4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl4.Visible = false;
+            this.lbl4.Click += new System.EventHandler(this.label_click);
+            // 
+            // lbl5
+            // 
+            this.lbl5.AutoSize = true;
+            this.lbl5.BackColor = System.Drawing.SystemColors.Control;
+            this.lbl5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbl5.Font = new System.Drawing.Font("Mistral", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl5.Location = new System.Drawing.Point(286, 119);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(135, 117);
+            this.lbl5.TabIndex = 5;
+            this.lbl5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl5.Visible = false;
+            this.lbl5.Click += new System.EventHandler(this.label_click);
+            // 
+            // lbl6
+            // 
+            this.lbl6.AutoSize = true;
+            this.lbl6.BackColor = System.Drawing.SystemColors.Control;
+            this.lbl6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbl6.Font = new System.Drawing.Font("Mistral", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl6.Location = new System.Drawing.Point(145, 119);
+            this.lbl6.Name = "lbl6";
+            this.lbl6.Size = new System.Drawing.Size(134, 117);
+            this.lbl6.TabIndex = 4;
+            this.lbl6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl6.Visible = false;
+            this.lbl6.Click += new System.EventHandler(this.label_click);
+            // 
+            // lbl7
+            // 
+            this.lbl7.AutoSize = true;
+            this.lbl7.BackColor = System.Drawing.SystemColors.Control;
+            this.lbl7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbl7.Font = new System.Drawing.Font("Mistral", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl7.Location = new System.Drawing.Point(4, 119);
+            this.lbl7.Name = "lbl7";
+            this.lbl7.Size = new System.Drawing.Size(134, 117);
+            this.lbl7.TabIndex = 3;
+            this.lbl7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl7.Visible = false;
+            this.lbl7.Click += new System.EventHandler(this.label_click);
+            // 
+            // lbl8
+            // 
+            this.lbl8.AutoSize = true;
+            this.lbl8.BackColor = System.Drawing.SystemColors.Control;
+            this.lbl8.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbl8.Font = new System.Drawing.Font("Mistral", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl8.Location = new System.Drawing.Point(286, 1);
+            this.lbl8.Name = "lbl8";
+            this.lbl8.Size = new System.Drawing.Size(135, 117);
+            this.lbl8.TabIndex = 2;
+            this.lbl8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl8.Visible = false;
+            this.lbl8.Click += new System.EventHandler(this.label_click);
+            // 
+            // lbl9
+            // 
+            this.lbl9.AutoSize = true;
+            this.lbl9.BackColor = System.Drawing.SystemColors.Control;
+            this.lbl9.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbl9.Font = new System.Drawing.Font("Mistral", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl9.Location = new System.Drawing.Point(145, 1);
+            this.lbl9.Name = "lbl9";
+            this.lbl9.Size = new System.Drawing.Size(134, 117);
+            this.lbl9.TabIndex = 1;
+            this.lbl9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl9.Visible = false;
+            this.lbl9.Click += new System.EventHandler(this.label_click);
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.BackColor = System.Drawing.SystemColors.Control;
+            this.lbl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbl.Font = new System.Drawing.Font("Mistral", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl.Location = new System.Drawing.Point(4, 1);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(134, 117);
+            this.lbl.TabIndex = 0;
+            this.lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl.Visible = false;
+            this.lbl.Click += new System.EventHandler(this.label_click);
+            // 
+            // player1Btn
+            // 
+            this.player1Btn.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.player1Btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.player1Btn.Font = new System.Drawing.Font("Monotype Corsiva", 14F, System.Drawing.FontStyle.Italic);
+            this.player1Btn.Location = new System.Drawing.Point(100, 120);
+            this.player1Btn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.player1Btn.Name = "player1Btn";
+            this.player1Btn.Size = new System.Drawing.Size(222, 40);
+            this.player1Btn.TabIndex = 1;
+            this.player1Btn.Text = "One Player ";
+            this.player1Btn.UseVisualStyleBackColor = false;
+            this.player1Btn.Click += new System.EventHandler(this.player1Btn_Click);
+            // 
+            // player2Btn
+            // 
+            this.player2Btn.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.player2Btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.player2Btn.Font = new System.Drawing.Font("Monotype Corsiva", 14F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.player2Btn.Location = new System.Drawing.Point(100, 200);
+            this.player2Btn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.player2Btn.Name = "player2Btn";
+            this.player2Btn.Size = new System.Drawing.Size(222, 40);
+            this.player2Btn.TabIndex = 2;
+            this.player2Btn.Text = "Two Players ";
+            this.player2Btn.UseVisualStyleBackColor = false;
+            this.player2Btn.Click += new System.EventHandler(this.player2Btn_Click);
+            // 
+            // NC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(425, 355);
+            this.Controls.Add(this.player2Btn);
+            this.Controls.Add(this.player1Btn);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.Name = "NC";
+            this.Text = "Naughts and Crosses";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Label lbl5;
+        private System.Windows.Forms.Label lbl6;
+        private System.Windows.Forms.Label lbl7;
+        private System.Windows.Forms.Label lbl8;
+        private System.Windows.Forms.Label lbl9;
+        private System.Windows.Forms.Button player1Btn;
+        private System.Windows.Forms.Button player2Btn;
+    }
+}
+
---
+++ NaughtsAndCrosses/NaughtsAndCrosses/NaughtsAndCrossesForm.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NaughtsAndCrosses
+{
+
+    public partial class NC : Form
+    {
+        Boolean p1Turn = true;
+        Boolean AI = false;
+
+        List<Label> labelAI = new List<Label>();
+
+
+        string txtPlayer1 = "X";
+        string txtPlayer2 = "O";
+
+        public String checkwin()
+        {
+            if (row().Equals("X") || column().Equals("X") || diagonal().Equals("X"))
+            {
+                return "X";
+            }
+            else if (row().Equals("O") || column().Equals("O") || diagonal().Equals("O"))
+            {
+                return "O";
+            }
+            else
+            {
+                return "NoWin";
+            }
+        }
+
+        public string row()
+        {
+            List<Label> row1 = new List<Label>();
+            row1.Add(lbl);
+            row1.Add(lbl7);
+            row1.Add(lbl4);
+
+            List<Label> row2 = new List<Label>();
+            row2.Add(lbl9);
+            row2.Add(lbl6);
+            row2.Add(lbl3);
+
+            List<Label> row3 = new List<Label>();
+            row3.Add(lbl8);
+            row3.Add(lbl5);
+            row3.Add(lbl2);
+
+            if ((row1.All(item => item.Text == "X")) || (row2.All(item => item.Text == "X")) || (row3.All(item => item.Text == "X")))
+            {
+                return "X"; 
+            }
+            else if ((row1.All(item => item.Text == "O")) || (row2.All(item => item.Text == "O")) || (row3.All(item => item.Text == "O")))
+            {
+                return "O";
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        public string column()
+        {
+            List<Label> column1 = new List<Label>();
+            column1.Add(lbl);
+            column1.Add(lbl9);
+            column1.Add(lbl8);
+
+            List<Label> column2 = new List<Label>();
+            column2.Add(lbl7);
+            column2.Add(lbl6);
+            column2.Add(lbl5);
+
+            List<Label> column3 = new List<Label>();
+            column3.Add(lbl4);
+            column3.Add(lbl3);
+            column3.Add(lbl2);
+
+            if ((column1.All(item => item.Text == "X")) || (column2.All(item => item.Text == "X")) || (column3.All(item => item.Text == "X")))
+            {
+                return "X";
+            }
+            else if ((column1.All(item => item.Text == "O")) || (column2.All(item => item.Text == "O")) || (column3.All(item => item.Text == "O")))
+            {
+                return "O";
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        public string diagonal()
+        {
+            List<Label> diagonal1 = new List<Label>();
+            diagonal1.Add(lbl);
+            diagonal1.Add(lbl6);
+            diagonal1.Add(lbl2);
+
+            List<Label> diagonal2 = new List<Label>();
+            diagonal2.Add(lbl8);
+            diagonal2.Add(lbl6);
+            diagonal2.Add(lbl4);
+
+            if ((diagonal1.All(item => item.Text == "X")) || (diagonal2.All(item => item.Text == "X")))
+            {
+                return "X";
+            }
+            else if ((diagonal1.All(item => item.Text == "O")) || (diagonal2.All(item => item.Text == "O")))
+            {
+                return "O";
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        public void endGame()
+        {
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+                if (iconLabel.Text.Equals(""))
+                {
+                    iconLabel.Visible = false;
+                }
+            }
+
+            //message box 
+            string message = "";
+            if (!checkwin().Equals("NoWin"))
+            {
+                message = "Congratulations! " + checkwin() + " wins! Do you want to have another game?";
+            }
+            else if (checkDraw())
+            {
+                message = "Congratulations! its a draw! Do you want to have another game?";
+            }
+
+            DialogResult dialogResult = MessageBox.Show(message, "End Game", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                player2Btn.Visible = true;
+                player1Btn.Visible = true;
+
+                tableLayoutPanel1.Visible = false;
+
+
+                foreach (Control control in tableLayoutPanel1.Controls)
+                {
+                    Label iconLabel = control as Label;
+
+                    iconLabel.Visible = false;
+                    iconLabel.Text = "";
+                }
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                this.Close();
+            } 
+        }
+
+        public bool checkDraw()
+        {
+            Boolean draw = true;
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+                if (iconLabel.Text.Equals(""))
+                {
+                    draw = false;
+                }
+            }
+            if (draw)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void startGame()
+        {
+            player2Btn.Visible = false;
+            player1Btn.Visible = false;
+
+            tableLayoutPanel1.Visible = true;
+
+
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+
+                iconLabel.Visible = true;
+                iconLabel.Text = "";
+            }
+        }
+
+        public void AIturn()
+        {
+            Random r1 = new Random();
+            if (labelAI.Any())
+            {
+                int selectedLbl = r1.Next(labelAI.Count);
+                labelAI[selectedLbl].Text = txtPlayer2;
+                labelAI.RemoveAt(selectedLbl);
+            }
+
+            //List<Label> avaliablelabels = new List<Label>();
+            //if there are two labels in a row that are o
+            //if there are two labels in a column that are x 
+            //check diagonals 
+            //tableLayoutPanel1.GetControlFromPosition(1,1);
+
+
+        }
+
+        public NC()
+        {
+            InitializeComponent();
+        }
+
+
+        private void label_click(object sender, EventArgs e)
+        {
+            Label clickedLabel = sender as Label;
+            if (clickedLabel != null)
+            {
+                if (AI == false)
+                {
+                    if (clickedLabel.Text.Equals(""))
+                    {
+                        if (p1Turn)
+                        {
+                            clickedLabel.Text = txtPlayer1;
+                            p1Turn = false;
+                            if (!checkwin().Equals("NoWin"))
+                            {
+                                endGame();
+                            }
+                            else if (checkDraw())
+                            {
+                                endGame();
+                            }
+                        }
+                        else
+                        {
+                            clickedLabel.Text = txtPlayer2;
+                            p1Turn = true;
+                            if (!checkwin().Equals("NoWin"))
+                            {
+                                endGame();
+                            }
+                            else if (checkDraw())
+                            {
+                                endGame();
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    Boolean end = false;
+
+                    if (clickedLabel.Text.Equals(""))
+                    {
+                            clickedLabel.Text = txtPlayer1;
+                            labelAI.Remove(clickedLabel);
+                            if (!checkwin().Equals("NoWin"))
+                            {
+                                endGame();
+                                end = true;
+                            }
+                            else if (checkDraw())
+                            {
+                                endGame();
+                                end = true;
+                            }
+
+                        if (!end)
+                        {
+                            AIturn();
+                            if (!checkwin().Equals("NoWin"))
+                            {
+                                endGame();
+                            }
+                            else if (checkDraw())
+                            {
+                                endGame();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void player2Btn_Click(object sender, EventArgs e)
+        {
+            startGame();
+            AI = false;
+        }
+
+        private void player1Btn_Click(object sender, EventArgs e)
+        {
+            startGame();
+            AI = true;
+
+            foreach (Label label in tableLayoutPanel1.Controls)
+            {
+                labelAI.Add(label);
+            }
+        }
+    }
+}
